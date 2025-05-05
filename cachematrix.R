53aab969a99c14537c68d0052df8d04828695e6a
jakrajesh576 <rajesh.jakkula@sanofi.com>
2025-05-05 17:47:50 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,46 @@
 
-makeCacheVector <- function(x = numeric()) {
-  m <- NULL # Initialize the cached mean to NULL
-  set <- function(y) {
-    x <<- y  # Assign the new vector to x in the parent environment
-    m <<- NULL  # Reset the cached mean to NULL
+# This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function() {
+  mat <- NULL
+  inv_cache <- NULL
+  
+  setMatrix <- function(x) {
+    mat <<- x
+    inv_cache <<- NULL
   }
-  get <- function() x  # Return the vector
-  setmean <- function(mean) m <<- mean  # Cache the mean
-  getmean <- function() m # Return the cached mean
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+  
+  getMatrix <- function() {
+    mat
+  }
+  
+  getInverse <- function() {
+    if (is.null(inv_cache)) {
+      inv_cache <<- solve(mat)
+    }
+    inv_cache
+  }
+  
+  list(setMatrix = setMatrix, getInverse = getInverse)
 }
 
-cacheMean <- function(x, ...) {
-  m <- x$getmean() # Retrieve the cached mean
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m) # Return the cached mean if it exists
-  }
-  data <- x$get() # Get the vector
-  m <- mean(data, ...) # Compute the mean
-  x$setmean(m) # Cache the mean
-  m # Return the computed mean
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix
+cacheSolve <- function(x, ...) {
+  inv_cache <- x$getInverse()
+  inv_cache
 }
 
-# Example usage
-v <- makeCacheVector(c(1, 2, 3, 4))
-print(cacheMean(v)) # Computes and caches the mean
-print(cacheMean(v)) # Retrieves the cached mean
+
+# Create a special "matrix" object
+special_matrix <- makeCacheMatrix()
+
+# Set the matrix
+special_matrix$setMatrix(matrix(c(1, 2, 3, 4), 2, 2))
+
+# Compute and cache the inverse
+inverse_matrix <- cacheSolve(special_matrix)
+print(inverse_matrix)
+
+# Retrieve the cached inverse
+cached_inverse <- cacheSolve(special_matrix)
+print(cached_inverse)
+
